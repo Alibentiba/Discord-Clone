2cd2ceac95d55f0e0d7deb6d84c3dd8a005c12de
unknown <bentibaali3@gmail.com>
2022-04-20 12:04:34 +0100

    Body coding

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { useState } from 'react'
 import data from './data'
 import Navbar from './Navbar/Navbar'
+import Body from './Body/Body'
 function App() {
   const [people, setPeople] = useState(data)
   return (
     <main>
       <Navbar/>
+      <Body/>
+
     </main>
   )
 }
